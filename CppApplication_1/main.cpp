aee3d83a791042608a70b96f5999c9d710d38285
mbouchett <mark@markbouchett.com>
2017-06-16 10:31:11 -0400

    starting point

---
+++ CppApplication_1/main.cpp
@@ -0,0 +1,24 @@
+/* 
+ * File:   main.cpp
+ * Author: Mark Bouchett
+ *
+ * Created on June 16, 2017, 10:29 AM
+ */
+
+#include <iostream>  //console Input/Output
+#include <string>  //string handler
+#include <fstream>  //file stream handler
+#include <unistd.h>             //unix standard header
+#include <termios.h>            //General Terminal Interface
+
+using namespace std; //declares the name space
+// declare function prototypes
+
+/*
+ * 
+ */
+int main() {
+
+    return 0;
+}
+
